96336d5bcc7f725202e35578d296fc23b7ff71df
seyfer <seyferseed@mail.ru>
2016-08-06 21:06:21 +0700

    learn further

--- src/main/java/seed/seyfer/controllers/ProductController.java
+++ src/main/java/seed/seyfer/controllers/ProductController.java
@@ -1,7 +1,12 @@
 package seed.seyfer.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import seed.seyfer.services.ProductService;
 
 /**
  * Created by seyfer on 8/3/16.
@@ -9,8 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController
 {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product")
     public String getProduct() {
+        return "redirect:/";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("product", productService.getProduct(id));
+
         return "product";
     }
 }
---
+++ src/main/java/seed/seyfer/services/ProductService.java
@@ -0,0 +1,16 @@
+package seed.seyfer.services;
+
+
+import seed.seyfer.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/26/16.
+ */
+public interface ProductService {
+
+    Product getProduct(Integer id);
+
+    List<Product> listProducts();
+}
