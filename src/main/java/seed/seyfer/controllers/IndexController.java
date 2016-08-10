1f4b42d8dc4156f177f0599b01d6d64dd2060f33
seyfer <seyferseed@mail.ru>
2016-08-10 21:32:20 +0700

    learn further

--- src/main/java/seed/seyfer/controllers/IndexController.java
+++ src/main/java/seed/seyfer/controllers/IndexController.java
@@ -1,7 +1,10 @@
 package seed.seyfer.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import seed.seyfer.services.ProductService;
 
 /**
  * Created by seyfer on 8/3/16.
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController
 {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/")
-    public String index() {
+    public String getIndex(Model model) {
+
+        model.addAttribute("products", productService.listProducts());
+
         return "index";
     }
 }
